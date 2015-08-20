077dfc85d9218130a9a71f69c98bccc7c6ee7205
Matthew Reisch <m.reisch@amalgamatedanalytics.com>
2015-08-20 18:51:56 -0400

    Removed french localizations.

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -9,18 +9,11 @@ sudo apt-get install -y curl flex bison zlib1g-dev libreadline6-dev gcc make sup
 sudo apt-get install -y python-dev
 sudo apt-get install -y pgxnclient
 
-### LOCALE
-
-sudo apt-get install -y locales && localedef -i fr_FR -c -f UTF-8 -A /usr/share/locale/locale.alias fr_FR.UTF-8
-sudo locale-gen fr_FR.UTF-8
-echo "export LANG=fr_FR.UTF-8" >> ~/.pgxl
-echo "export LANGUAGE=fr_FR:fr" >> ~/.pgxl
-echo "LC_ALL=fr_FR.UTF-8" >> ~/.pgxl
 
 ### PGXL
 
 cd /tmp
-git clone https://github.com/postmind-net/postgres-xl.git pgxl --depth 1
+git clone https://github.com/mbreisch/postgres-xl.git pgxl --depth 1
 cd /tmp/pgxl
 PATH=$PATH:/opt/anaconda/bin ./configure --with-python --prefix /opt/pgxl
 make -j 4
